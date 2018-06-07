3fccb552b97fe22b22acdf1b85cd1dfddc0f7365
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-07 13:32:59 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'stage-1']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+};
