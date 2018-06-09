37ee184fff88f3925af8cd3869a412b869534db6
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-09 11:30:51 -0700

    added this.state for updating a new value whenever an input is done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,12 +3,20 @@ import React, { Component }from 'react';
 // class-based component search bar 
 // giving search bar functionality from react.component class
 class SearchBar extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state ={ term: '' };
+    }
     render() {
-        // create a new input element and pass it as a props, property onChange with
-        // a value of this.onInputChange
-        // will be triggers whenever events occurs
-        // use of arrow function for ES6 and simplification
-        return <input onChange={ event => console.log(event.target.value)} />
+        return (
+            <div>
+            {/* whenever this is update, 'this.setState' will set a new value of our 'term' input
+            it causes our component to automatically rerender and push all those updated values into the DOM  */}
+              <input onChange={event => this.setState({ term: event.target.value })} />
+              Value of the input: {this.state.term}
+            </div>
+        )
     }
 }
 
