3cc3ff61f2a480c1abf4a02adc538f4337e576eb
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-09 22:46:23 -0700

    added video image and title

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({video}) => {
+    // arguments has a property video, same with the syntax below
+    // above is an ES6 syntax
+    // const video = props.video;
+
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}/>
+                </div>
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
