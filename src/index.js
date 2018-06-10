0be41457de132636e7f4ac190bb3502effdbca2d
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-09 21:57:20 -0700

    modified index.js and changed functional component to class components and implement ES6 syntax inside constructor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-//api key for youtube
 const API_KEY = "AIzaSyA_A87KhTJlWXKOadXwDVPDckE1_pWAb-I";
 
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-//Create new component. This component will produce some HTML
-const App = () => {
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+            this.setSate({ videos });
+            //this.setState({ videos: videos})
+            //in ES6, when key and value are the same can be optimized as the above
+        });
+    }
+  render() {
     return (
         <div>
             <SearchBar />
         </div>
     );
+  }
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
