30da628fd16cdc6b28fd819a5260303a7f4e1e73
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-09 22:32:53 -0700

    added List Item Keys as a unique identifier for each list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,9 +4,9 @@ import VideoListItem from './video_list_item';
 //props here is an argument because it is a functional component
 const VideoList = (props) => {
    const videoItems = props.videos.map((video) => {
-        return <VideoListItem video={video} />
+        return <VideoListItem key={video.etag} video={video} />
     });
-    
+
     return ( 
         <ul className="col-md-4 list-group">
             {videoItems}
